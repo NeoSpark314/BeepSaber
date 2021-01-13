d6fb1ad3a5a23f4675c0e51fea2527a6cf87b2ec
leandrodreamer <leandrodreamer1@gmail.com>
2021-01-13 18:12:54 -0500

    show cube points
    show cube cut points
    reuse audiostreamplayer from points objects
    limit point objects and cutted pieces
    collision with floor reenabled

---
+++ game/Points_label_driver.gd
@@ -0,0 +1,26 @@
+extends Spatial
+
+
+var points_label=[]
+export var points_label_amount = 4
+onready var points_label_ref = preload("res://game/points_label.tscn")
+var current_point_label = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(0,points_label_amount):
+		points_label.insert(0,points_label_ref.instance())
+		add_child(points_label[0])
+
+
+func show_points(position=Vector3(),value=0):
+	var color = Color(1,1,1)
+	if value == 0:
+		value = "x"
+		color = Color(1,0,0)
+	else:
+		color = Color(1-clamp(value/150,0.4,1),clamp(value/150,0.4,1),0)
+	points_label[current_point_label].show_points(position,value,color)
+	current_point_label += 1
+	current_point_label %= points_label.size()
---
+++ game/points_label.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+
+func show_points(position=Vector3(),value="x",color = Color(1,1,1)):
+	transform.origin = position
+	$Viewport/Label.text = str(value)
+	$Viewport/Label.modulate = color
+	$AnimationPlayer.stop()
+	$AnimationPlayer.play("hit")
+	$AudioStreamPlayer3D.play()
