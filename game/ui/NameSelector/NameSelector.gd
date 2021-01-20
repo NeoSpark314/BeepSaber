5f84069592444664245bf8e6eaef2b0e8a58076e
hankedan000 <dany32412@gmail.com>
2021-01-19 22:11:59 -0500

    Creates NameSelector

---
+++ game/ui/NameSelector/NameSelector.gd
@@ -0,0 +1,22 @@
+extends Control
+
+onready var _base_button = $NameRow/BaseButton.duplicate()
+onready var _name_row = $NameRow
+
+func _ready():
+	clear_names()
+
+# adds a name button to the list
+# names that are added first show up first in list
+func add_name(name):
+	var new_button = _base_button.duplicate()
+	new_button.text = name
+	new_button.connect("pressed",self,"_on_NameButton_pressed",[name])
+	_name_row.add_child(new_button)
+
+func clear_names():
+	for child in _name_row.get_children():
+		child.queue_free()
+
+func _on_NameButton_pressed(name):
+	print(name)
