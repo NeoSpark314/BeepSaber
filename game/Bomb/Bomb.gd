1f170d0914bfa0a3d87065137506031779d9b109
hankedan000 <dany32412@gmail.com>
2021-07-02 00:01:16 -0400

    Adds spawn animation

--- game/Bomb/Bomb.gd
+++ game/Bomb/Bomb.gd
@@ -1,1 +1,9 @@
 extends Note
+
+export var min_speed = 0.5;
+onready var _anim = $BombAnimation/AnimationPlayer
+
+func _ready():
+	# play the spawn animation when this bomb enters the scene
+	_anim.playback_speed = max(min_speed,speed)
+	_anim.play("Spawn");
