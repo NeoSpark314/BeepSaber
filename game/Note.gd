2afa81e69db9d83cea67026cf4e16ed4a6e3a4e9
hankedan000 <dany32412@gmail.com>
2021-07-02 15:18:02 -0400

    Adds documentation to Note

--- game/Note.gd
+++ game/Note.gd
@@ -1,5 +1,12 @@
 extends Spatial
 class_name Note
 
+# strucutre of information sourced from the map file for this note instance
+# https://bsmg.wiki/mapping/map-format.html#notes-2
 var _note;
+
+# the note's velocity toward the player
+# currently set to _noteJumpMovementSpeed / 9.0 in main game. I'm not sure what
+# the 9.0 is for...
+# units: meters / second
 var speed = 1.0;
