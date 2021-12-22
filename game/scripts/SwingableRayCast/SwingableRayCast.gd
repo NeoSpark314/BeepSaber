91899c0bf3d51bbcb5b86d47e5e25d734c2ae00f
hankedan000 <dany32412@gmail.com>
2021-12-22 10:39:27 -0500

    Adds min length check to SwingableRayCast. Fixes collision mask set bug.

--- game/scripts/SwingableRayCast/SwingableRayCast.gd
+++ game/scripts/SwingableRayCast/SwingableRayCast.gd
@@ -4,11 +4,18 @@ extends RayCast
 
 signal area_collided(area)
 
-export (int,2,10) var num_collision_raycasts = 6
+export (int,2,10) var num_collision_raycasts = 8
 
 const DEBUG = false
 const DEBUG_TRAIL_SEGMENTS = 5
 const LinkedList = preload("res://game/scripts/LinkedList.gd")
+
+# this constant is used to prevent unnecessary raycast collision computations
+# when the ray's 'cast_to' vector length is below this threshold. it seems like
+# Godot's physics engine never picks up on collisions when the vector's length
+# is below this threshold. this vlaue was imperically found by logging the
+# minimum length when using the node in-game.
+const MIN_SWEPT_LENGTH_THRESHOLD = 0.035
 
 # the type of note this saber can cut (set in the game main)
 var _prev_ray_positions = [];
@@ -21,7 +28,7 @@ func _ready():
 	yield(get_tree(),"physics_frame")
 	
 	# use discrete RayCasts for continuous collision detection between _physics_process()
-	for i in range(num_collision_raycasts):
+	for _i in range(num_collision_raycasts):
 		var new_ray := RayCast.new()
 		# inherit properties of parent
 		new_ray.collision_mask = collision_mask
@@ -41,7 +48,13 @@ func _ready():
 	# no longer need original instance
 	remove_child($debug_ball)
 
-func _physics_process(delta):
+# override so that we can update child segments too
+func set_collision_mask_bit(bit: int, value: bool):
+	collision_mask = collision_mask | (int(value) << bit)
+	for ray in _rays:
+		ray.set_collision_mask_bit(bit,value)
+
+func _physics_process(_delta):
 	_sw.start()
 	# see if 'core' ray is colliding with anything
 	var coll = get_collider()
@@ -69,10 +82,11 @@ func _physics_process(delta):
 			
 		# cast a ray to the newest location and check for collisions
 		ray.cast_to = ray.to_local(_prev_ray_positions[i])
-		ray.force_raycast_update()
-		coll = ray.get_collider()
-		if coll is Area:
-			emit_signal("area_collided",coll)
+		if ray.cast_to.length() > MIN_SWEPT_LENGTH_THRESHOLD:
+			ray.force_raycast_update()
+			coll = ray.get_collider()
+			if coll is Area:
+				emit_signal("area_collided",coll)
 		
 		_prev_ray_positions[i] = next_global_pos
 		next_local_pos += step_dist
@@ -92,9 +106,9 @@ func _on_SwingableRayCast_tree_entered():
 	if DEBUG:
 		var root = get_tree().get_root()
 		var scene_root = root.get_child(root.get_child_count() - 1)
-		for t in range(DEBUG_TRAIL_SEGMENTS):
+		for _t in range(DEBUG_TRAIL_SEGMENTS):
 			var trail_slice = []
-			for i in range(num_collision_raycasts):
+			for _i in range(num_collision_raycasts):
 				var new_ray := RayCast.new()
 				new_ray.enabled = true
 				new_ray.collision_mask = collision_mask
